80009221e986ac5f4eba9a368c5ed96c2bb7bcd2
dRumata <rumata@ya.ru>
2022-07-01 16:50:44 +0500

    com

--- 1602/bash-history/history.sh
+++ 1602/bash-history/history.sh
@@ -1,1 +1,3 @@
 wget https://download.astralinux.ru/astra/stable/smolensk/security-updates/1.6/20211126SE16/20211126SE16.iso
+sudo mkdir /srv/ftp/pub/upd10
+sudo mount /home/student/20211126SE16.iso /srv/ftp/pub/upd10
