5fcaaa9d3fc1ab767bb1db46391261cc554f3ab2
dRumata <rumata@ya.ru>
2022-07-04 10:51:34 +0500

    Create history.sh

---
+++ 1602/LAB2/history.sh
@@ -0,0 +1,7 @@
+wget https://download.astralinux.ru/astra/stable/smolensk/security-updates/1.6/20211126SE16/20211126SE16.iso
+sudo mkdir /srv/ftp/pub/upd10
+sudo mount /home/student/20211126SE16.iso /srv/ftp/pub/upd10
+
+sudo dpkg -i astra-update****.deb
+sudo vim /etc/apt/sources.list
+
